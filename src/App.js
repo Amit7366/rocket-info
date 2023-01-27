6893dd31f480c6f287cb2acded74870def95d481
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2023-01-28 01:31:22 +0600

    making card layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { Avatar, Card, Col, Row } from 'antd';
+import Meta from 'antd/es/card/Meta';
+import { useState } from 'react';
 
 function App() {
+  const [rockets,setRockets] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Row>
+        <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+        <Card
+    style={{
+      width: '100%'
+    }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8} xl={6}>col-6</Col>
+        <Col xs={24} sm={24} md={12} lg={8} xl={6}>col-6</Col>
+        <Col xs={24} sm={24} md={12} lg={8} xl={6}>col-6</Col>
+
+        
+      </Row>
     </div>
   );
 }
